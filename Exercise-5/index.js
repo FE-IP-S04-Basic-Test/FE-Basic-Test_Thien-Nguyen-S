aea45a2fadfc0c7ded6fcd29aa34476d252157c4
st-thiennguyen <thien.nguyen@supremetech.vn>
2023-08-22 17:03:10 +0700

    update js2 in index.js

--- Exercise-5/index.js
+++ Exercise-5/index.js
@@ -23,26 +23,63 @@ handleAddtoCart(productData[1], iceData[2], sugarData[2], 1);
 // Cau 2
 const twoArrayOfProduct = (arrA, arrB) => {
   let result = [];
-  for (const a of arrB) {
-    for (const b of arrA) {
-      if (a.id === b.id) {
+  for (const a of arrA) {
+    for (const b of arrB) {
+      if (b.id === a.id) {
+        const check = result.find((i) => i.id === a.id);
         const id = a.id;
         const quantity = a.quantity + b.quantity;
-        const check = result.find((i) => i.id === a.id);
         if (!check) {
           result.push({ id, quantity });
         } else {
-          check.quantity += b.quantity;
+          check.quantity += quantity;
         }
       }
     }
   }
+  return result;
+};
+
+const twoArray = (arrA, arrB) => {
+  const quantitiesArr1 = {};
+  const quantitiesArr2 = {};
+  let result = [];
+
+  for (const value of arrA) {
+    const id = value.id;
+    const quantity = value.quantity;
+    if (!quantitiesArr1[id]) {
+      quantitiesArr1[id] = 0;
+    }
+    quantitiesArr1[id] += quantity;
+  }
+
+  console.log(quantitiesArr1);
+
+  for (const value of arrB) {
+    const id = value.id;
+    const quantity = value.quantity;
+    if (!quantitiesArr2[id]) {
+      quantitiesArr2[id] = 0;
+    }
+    quantitiesArr2[id] += quantity;
+  }
+
+  console.log(quantitiesArr2);
+
+  for (const key in quantitiesArr2) {
+    if (quantitiesArr1[key]) {
+      result.push({ id: key, quantity: quantitiesArr1[key] + quantitiesArr2[key] });
+    }
+  }
+
+  console.log(result);
 };
 
 const arrA = [
   { id: '1', quantity: 2 },
   { id: '2', quantity: 2 },
-  { id: '3', quantity: 3 },
+  { id: '3', quantity: 2 },
   { id: '1', quantity: 3 }
 ];
 
@@ -51,4 +88,6 @@ const arrB = [
   { id: '3', quantity: 2 }
 ];
 
-console.log(twoArrayOfProduct(arrA, arrB));
+twoArray(arrA, arrB);
+
+// console.log(twoArrayOfProduct(arrA, arrB));
